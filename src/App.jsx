ded9a15950a1ba7e439693e1226548b14f7d266f
kaikaew13 <ksaratunti@gmail.com>
2022-10-14 18:43:15 +0700

    add files for main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
+
+import About from './components/About';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+
 function App() {
   return (
     <div className='App'>
-      <h1 className=' text-gradient font-poppins text-[46px]'>hello</h1>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   );
 }
