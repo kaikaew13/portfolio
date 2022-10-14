aa6a0660fbfa95577cefb4e29e5b2ce1a7a762b5
kaikaew13 <ksaratunti@gmail.com>
2022-10-14 18:03:20 +0700

    configure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'san-serif'],
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
   },
   plugins: [],
-}
+};
